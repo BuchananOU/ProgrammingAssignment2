07aa48942accdf1c3dd9206eeae16c3923caa80a
Buchanan <BuchananOU@users.noreply.github.com>
2015-05-23 14:19:43 -0500

    Update cachematrix.R
    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2: based on the special vector functions, these make, cache, and solve special matrices.
 
-## Write a short comment describing this function
+## creates a special matrix that can cache its own inverse (if solvable).  uses a list of functions and the matrix "solve" 
+## function  NOTE: only square matrices will be solvable by solve()
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -16,8 +16,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function - returns the inverse of a special matrix from makeCacheMatrix. 
-## If cached, it uses that value.
+## returns the inverse of a special matrix from makeCacheMatrix. 
+## If cached, it uses that value instead of computing it again.  If not, it will compute and cache the value
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
